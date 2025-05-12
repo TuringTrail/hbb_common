6f64fdb7483402ce15c29a509a99c71e03ff7451
Turing <turing@xusw.com>
2025-05-12 21:42:12 +0800

    修改中继服务器域名和密钥

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.xusw.com"];
+pub const RS_PUB_KEY: &str = "WUlIyYc5jVf+mdFgW1Stoo8gGmatWx3UKl0CePEbo9k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
